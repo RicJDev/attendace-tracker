b18963c93abfcd6ed2ef86cfcb65f7bb6b4d720b
RicJDev <ric.mmosqueda@gmail.com>
2026-04-06 16:02:13 -0400

    refactor: mejora de legibilidad en el boton

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -2,11 +2,12 @@ import './Button.css'
 import type { ButtonHTMLAttributes } from 'react'
 
 export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+  const { className, ...rest } = props
   return (
     <>
       <button
-        {...props}
-        className={`custom-button ${props.className}`}
+        {...rest}
+        className={`custom-button ${className ?? ''}`}
       ></button>
     </>
   )
