d041553d99cf7e16597bf2577a06f5384a191a31
RicJDev <ric.mmosqueda@gmail.com>
2026-04-06 15:18:09 -0400

    refactor: simplificando header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,12 @@
 import './Header.css'
-import { Button } from '../components/Button'
-import { useNavigate } from 'react-router'
+import { NavLink } from 'react-router'
 
 export function Header() {
-  const navigate = useNavigate()
-
   return (
     <header className='main-header'>
-      <Button onClick={() => navigate(-1)}>{'<'}</Button>
-      <h1>Registro de asistencias</h1>
-      <Button onClick={() => navigate(1)}>{'>'}</Button>
+      <NavLink to='/'>
+        <h1>Registro de asistencias</h1>
+      </NavLink>
     </header>
   )
 }
