01266fea27a8e0240f527825d399505755a7479d
RicJDev <ric.mmosqueda@gmail.com>
2026-04-09 15:46:50 -0400

    feat: agregando layout nuevo para las secciones

---
+++ client/src/layouts/SectionLayout.tsx
@@ -0,0 +1,18 @@
+import './SectionLayout.css'
+import type { PropsWithChildren } from 'react'
+
+interface SectionLayoutProps extends PropsWithChildren {
+  name: string
+}
+
+export function SectionLayout({ name, children }: SectionLayoutProps) {
+  return (
+    <div className='section-layout'>
+      <header>
+        <h2>{name}</h2>
+      </header>
+
+      <article>{children}</article>
+    </div>
+  )
+}
