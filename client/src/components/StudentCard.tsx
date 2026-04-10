ccbb2f0dcb421a60cbee9c5f536be6930b31c1bb
RicJDev <ric.mmosqueda@gmail.com>
2026-04-09 20:36:20 -0400

    style: no se como estilar este componente. Matenme
    En serio, en serio... cual es el punto de todo esto?

--- client/src/components/StudentCard.tsx
+++ client/src/components/StudentCard.tsx
@@ -1,12 +1,12 @@
-import "./StudentCard.css"
+import './StudentCard.css'
 import type { Student as StudentType } from '../types'
 
-export function StudentCard({ name, lastName, listIndex, id }: StudentType) {
+export function StudentCard({ name, lastName, listIndex }: StudentType) {
   return (
     <div className='student-card'>
-      <p>Estudiante #{listIndex}</p>
-      <p>{id}</p>
-      <p>{name} {lastName}</p>
+      <p>
+        #{listIndex}: {name} {lastName}
+      </p>
     </div>
   )
 }
