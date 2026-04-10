e12a44c9d09f9f9666451317972c2f39cdaa77b7
RicJDev <ric.mmosqueda@gmail.com>
2026-04-09 20:34:55 -0400

    feat: agregando pantalla de carga

--- client/src/pages/Daily.tsx
+++ client/src/pages/Daily.tsx
@@ -1,35 +1,49 @@
 import './Daily.css'
-import type { Student as StudentType } from '../types'
 import { useEffect, useState } from 'react'
-import { StudentCard } from '../components/StudentCard'
-import { SectionLayout } from '../layouts/SectionLayout'
 import { getStudents } from '../services/studentsService'
 import { useNavigate } from 'react-router'
+// import { StudentCard } from '../components/StudentCard'
+import { SectionLayout } from '../layouts/SectionLayout'
 import { StudentList } from '../components/StudentList'
-
-interface DailyProps {
-  students: StudentType[]
-}
+import { Button } from '../components/Button'
+import { Loading } from '../layouts/Loading'
+import type { Student as StudentType } from '../types'
 
 /*
-- [ ] Obtener el listado de estudiantes desde el servidor
-- [ ] Mostrar pantalla de carga
+TODO:
+- [-] Obtener el listado de estudiantes desde el servidor
+- [x] Mostrar pantalla de carga
 - [ ] Indicar que se puede empezar
-- [ ] Mencionar los estudiantes uno a uno
+- [x] Mencionar los estudiantes uno a uno
 - [ ] Registrar el estatus de su asistencia
 - [ ] Enviar los datos al servidor
 */
 
-export function Daily({}: DailyProps) {
+export function Daily() {
+  const date = new Date()
+  const name = `Registro Diario (${date.getDate()}/${date.getMonth()}/${date.getFullYear()})`
   const [students, setStudents] = useState<StudentType[]>([])
   const [current, setCurrent] = useState(0)
   const [isLoading, setIsLoading] = useState(true)
   const navigate = useNavigate()
 
-  const handleNext = () => {
-    setCurrent(current + 1)
+  const handleSend = () => {
+    alert(
+      'Los datos se estan mandando... no se a donde, pero se estan mandando',
+    )
+    navigate('/')
+  }
+  const handleRestart = () => {
+    const restart = confirm(
+      'Se reiniciara el registro. Los datos ingresados se perderan',
+    )
+
+    if (restart) {
+      setCurrent(0)
+    }
   }
 
+  const handleNext = () => setCurrent(current + 1)
   useEffect(() => {
     getStudents().then((data) => {
       setStudents(data)
@@ -37,41 +51,36 @@ export function Daily({}: DailyProps) {
     })
   })
 
-  if (isLoading) {
-    return (
-      <SectionLayout name='Registro Diario'>
-        <p>Cargando data de los estudiantes...</p>
-      </SectionLayout>
-    )
-  }
+  if (isLoading)
+    return <Loading message='Cargando data de los estudiantes...' />
 
   return (
-    <>
-      <SectionLayout name='Registro Diario'>
-        <div className='daily'>
-          {current < students.length ? (
-            <>
-              <StudentList students={students} />
-              <StudentCard {...students[current]} />
+    <SectionLayout name={name}>
+      <article className='daily-container'>
+        <StudentList students={students} />
 
-              <div className='button-panel'>
-                <button onClick={handleNext}>Asistente</button>
-                <button onClick={handleNext}>Inasistente</button>
-                <button onClick={handleNext}>Justificado</button>
-              </div>
-            </>
-          ) : (
-            <>
-              <p>No hay mas estudiantes para registrar</p>
-              <button onClick={() => setCurrent(0)}>
-                Empezar registro nuevamente
-              </button>
+        {current < students.length ? (
+          <div className='daily-panel'>
+            {/* <StudentCard {...students[current]} /> */}
+            <p>{`${students[current].listIndex}: ${students[current].name} ${students[current].lastName}`}</p>
 
-              <button onClick={() => navigate('/')}>Enviar registro</button>
-            </>
-          )}
-        </div>
-      </SectionLayout>
-    </>
+            <div className='buttons'>
+              <Button onClick={handleNext}>Asistente</Button>
+              <Button onClick={handleNext}>Inasistente</Button>
+              <Button onClick={handleNext}>Justificado</Button>
+            </div>
+          </div>
+        ) : (
+          <div className='daily-panel'>
+            <p>Registro diario completado!</p>
+
+            <div className='buttons'>
+              <Button onClick={handleRestart}>Reiniciar registro</Button>
+              <Button onClick={handleSend}>Enviar registro</Button>
+            </div>
+          </div>
+        )}
+      </article>
+    </SectionLayout>
   )
 }
