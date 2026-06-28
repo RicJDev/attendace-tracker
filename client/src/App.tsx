961541d148404b00ff1902822ed8973264a96865
Ricardo J. M. Mosqueda <ric.mmosqueda@gmail.com>
2026-06-28 09:40:04 -0400

    feat: probando mis componentes nuevos... de chill

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,25 @@
+import { Button } from './components/ui/Button'
+import { Container } from './layouts/Container'
+import { SimpleBox } from './layouts/SimpleBox'
+import { Stack } from './layouts/Stack'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+
 export default function App() {
-  return <h1>Hello world!</h1>
+  return (
+    <Container>
+      <Header />
+
+      <SimpleBox grow>
+        <Stack>
+          <Button>Do something</Button>
+          <Button variant='secondary'>Click me!</Button>
+          <Button variant='tertiary'>Next page</Button>
+          <Button variant='alert'>Delete</Button>
+        </Stack>
+      </SimpleBox>
+
+      <Footer />
+    </Container>
+  )
 }
