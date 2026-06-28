664e5603b979b9a43c1646cd9f3fac0560d1650d
Ricardo J. M. Mosqueda <ric.mmosqueda@gmail.com>
2026-06-28 09:39:34 -0400

    feat: agregando algunos componentes de chill

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import { SimpleBox } from '../layouts/SimpleBox'
+
+export function Footer() {
+  return (
+    <SimpleBox>
+      <h1>Hello world!</h1>
+    </SimpleBox>
+  )
+}
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { SimpleBox } from '../layouts/SimpleBox'
+
+export function Header() {
+  return (
+    <SimpleBox>
+      <h1 className='text-2xl'>Hello world!</h1>
+    </SimpleBox>
+  )
+}
