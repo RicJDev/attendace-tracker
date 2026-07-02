e2acdd1c45fe02a6625eb2d0e44ff12a5259b11e
Ricardo J. M. Mosqueda <ric.mmosqueda@gmail.com>
2026-07-02 16:54:48 -0400

    style: centrando cositas

--- client/src/layouts/Container.tsx
+++ client/src/layouts/Container.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from 'react'
 export function Container({ children }: PropsWithChildren) {
   return (
     <div
-      className='flex flex-col gap-2 min-h-screen w-full p-3 bg-gray-800 items-center'
+      className='flex flex-col gap-2 min-h-screen w-full p-3 bg-gray-800 items-center justify-center text-gray-50'
     >
       {children}
     </div>
